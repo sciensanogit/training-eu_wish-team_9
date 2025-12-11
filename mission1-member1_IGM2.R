5f5c9dbd6248fefd48995b52f2f049127e7677e1
Iria <iriagonzalez@usal.es>
2025-12-11 17:12:48 +0100

    Mission1 and 2 updated

--- mission1-member1_IGM2.R
+++ mission1-member1_IGM2.R
@@ -1,3 +1,82 @@
 # msg of sucess run
 cat("- This script was coded by member 1 \n")
 
+# msg of sucess run
+cat("- This script was coded by member 1 \n")
+
+# coding a line
+datana <- sum(1:10)
+datana2 <- sum(1:20)
+datana2
+
+install.packages("usethis")
+usethis::use_git_config(
+  user.name = "Iria",  # <-- Escribe tu nombre aquí
+  user.email = "iriagonzalez@usal.es" # <-- Escribe el email de tu cuenta de GitHub
+)
+
+##########################  Mission 2 - 01_  ############################### #
+# Aim ----
+#| load data and produce first graph
+# NOTES:
+#| list of things to do
+#|
+############################################################################### #
+
+
+############################################################################## #
+
+
+# Load packages 
+# select packages
+pkgs <- c("dplyr", "tidyr", "zoo", "writexl", "ggplot2")
+# install packages
+install.packages(setdiff(pkgs, rownames(installed.packages())))
+invisible(lapply(pkgs, FUN = library, character.only = TRUE))
+
+# load data ----
+# Belgian data are available here https://www.geo.be/catalog/details/9eec5acf-a2df-11ed-9952-186571a04de2?l=en
+#| Metadata
+#| siteName is the name of the treatment plant
+#| collDTStart is the date of sampling
+#| labName is the name of the lab analysing the sample
+#| labProtocolID is the protocol used to analyse the dample
+#| flowRate is the flow rate measured at the inlet of the treatment plant during sampling
+#| popServ is the population covered by the treatment plant
+#| measure is the target measured
+#| value is the result
+
+# sars-cov-2 data
+df_sc <- read.csv("https://data.geo.be/ws/sciensano/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=sciensano:wastewatertreatmentplantscovid&outputFormat=csv")
+
+# pmmv data
+df_pmmv <- read.csv("https://data.geo.be/ws/sciensano/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=sciensano:wastewatertreatmentplantspmmv&outputFormat=csv")
+
+# join both
+df <- df_sc %>%
+  rbind(df_pmmv)
+
+# clean data
+df <- df %>%
+  select(siteName, collDTStart, labName, labProtocolID, flowRate, popServ, measure, value)
+
+# format date
+df$date <- as.Date(df$date)
+
+# set and subset dates
+date_reporting <- as.Date("2025-09-01", format = "%Y-%m-%d")
+date_graph_start <- as.Date("2024-09-01", format = "%Y-%m-%d")
+date_graph_end <- as.Date("2025-12-01", format = "%Y-%m-%d")
+
+# subset sars and pmmv data based on labProtocolID used betwen date_start and date_end
+# display existing labProtocolID
+# unique(df$labProtocolID)
+
+# rename measures
+# diplay existing measure
+# unique(df$measure)
+
+# translate siteName to english
+
+
+cat("- Success : data prep \n")
