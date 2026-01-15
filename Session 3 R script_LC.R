eec07204f297e7e3ab6d1c2f32b946ced968c992
Luís Filipe Sá Ribeiro Coelho <luis.coelho@insa.min-saude.pt>
2026-01-15 14:41:28 +0000

    commit

---
+++ Session 3 R script_LC.R
@@ -0,0 +1,53 @@
+## Adapt 02_visuals.R code to
+#- load data saved in .data/Belgium_export-nation.csv. This csv will be created by running 01_data_prep-solution.R. If you cannot run 01_data_prep-solution.R, you can load the ./Belgium_expert-nation.csv
+chek
+#- create ./plot folder if not existing
+check
+#- create a figure with the viral ratio (variable called value_pmmv) at the national level (inspiration from mission2.R is welcome)
+
+  #convert "date" from character to date
+BelgiumExportNation <- BelgiumExportNation %>% 
+  mutate(date = as.Date(date))
+
+PlotPMMoVRatio <- BelgiumExportNation %>% 
+  select(date, value_pmmv) %>%
+  filter(!is.na(value_pmmv)) %>% 
+  ggplot(aes(x = date, y = value_pmmv, group = 1)) + 
+  geom_point(color = "orange2") +
+  geom_line(color = "orange2") +
+  labs(
+    x = "Date",
+    y = "PMMoV viral ratio",
+  ) +
+  theme_minimal() +
+  scale_x_date(
+    date_breaks = "2 month",
+    date_labels = "%b %Y"
+  )
+
+PlotPMMoVRatio
+  
+#- save graph as ./plot/graph-viral_ratio-nation.png
+
+check
+
+#- Display nice xaxis, yaxis
+
+check previous step
+
+#- Add a past two weeks moving average line from the variable  (variable called value_pmmv_avg14d_past)
+
+PlotPMMoVRatio_withpmmvAvg14d <- BelgiumExportNation %>% 
+  select(date, value_pmmv, value_pmmv_avg14d_past) %>%
+  filter(!is.na(value_pmmv) & !is.na(value_pmmv_avg14d_past)) %>% 
+  ggplot(aes(x = date,group = 1)) + 
+  geom_point(aes(y = value_pmmv), color = "orange2") +
+  geom_line(aes(y = value_pmmv), color = "orange2") +
+  geom_line(aes(y = value_pmmv_avg14d_past), color = "lightblue") + 
+  labs(
+    x = "Date",
+    y = "PMMoV viral ratio",
+  ) +
+  theme_minimal()
+
+PlotPMMoVRatio_withpmmvAvg14d
